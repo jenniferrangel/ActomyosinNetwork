9afa21e49566107b1aa08bc914d30be62c0c488f
Jennifer Rangle <jrang008@bluejay.ib.hpcc.ucr.edu>
2024-01-29 17:40:03 -0800

    setup externs

--- externs.h
+++ externs.h
@@ -8,8 +8,29 @@
 
 //Actin parameters:
 //============================================
+//parameters for linear springs:
+extern double ACTIN_SPRING_EQUI_LEN;
+extern double K_LINEAR_STIFF_ACTIN;
+//parameters for bending springs:
+extern double K_BEND_STIFF_ACTIN;
+extern double THETA_EQUI_ANGLE;
+extern double ACTIN_DRAG_COEFF;
+extern double VISCOSITY_CYTOPLASM;
+extern double ACTIN_PERSISTENCE_LEN;
 
 //Myosin parameters:
 //============================================
+extern double F_MYO_PULLING;
+extern double MYO_DRAG_COEFF;
+
+//Actomyosin parameters:
+//============================================
+extern double ACTIN_MYO_CONNECT_RADIUS;
+extern double K_LINEAR_STIFF_ACTIN_MYO_CONN;
+extern double ACTIN_MYO_CONN_SPRING_EQUI_LEN;
+
+extern double kB;
+extern double TEMPERATURE;
+extern double dt;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -31,13 +31,39 @@ using namespace std;
 
 //Actin parameters:
 //============================================
+//parameters for linear springs:
+double ACTIN_SPRING_EQUI_LEN = 0.2;     //(microns) actin linear spring equilibrium length
+double K_LINEAR_STIFF_ACTIN = 150.0;     //(pN) The actin linear spring coefficient
+//parameters for bending springs:
+double K_BEND_STIFF_ACTIN = 0.0414;     //(pN*micron^2) The actin bending coefficientat every node triplet
+double THETA_EQUI_ANGLE = 3.14159265;  // The equilibrium angle which is pi
+double ACTIN_DRAG_COEFF = 0.216;        //(pN s/micron)
+double VISCOSITY_CYTOPLASM = 0.301;     //(Pa s OR pN s/micron)
+double ACTIN_PERSISTENCE_LEN = 10.0;    //(microns)    //(microns)
 
 //Myosin parameters:
 //============================================
+double F_MYO_PULLING = 4.0;        //(pN) myosin pulling force
+double MYO_DRAG_COEFF = 400.0;     //(pN s/micron) myosin drag coefficient
+
+//Actomyosin parameters:
+//============================================
+double ACTIN_MYO_CONNECT_RADIUS = 0.15;  //(microns) actin and myosin need to be within this distance to form a connection
+double K_LINEAR_STIFF_ACTIN_MYO_CONN = 2.0;  //(pN/micron) spring constant for the actin-myosin connection
+double ACTIN_MYO_CONN_SPRING_EQUI_LEN = 0.0;  //(microns) equilibrium length of actin-myosin connection (linear spring)
+
+double kB = 1.38064852e-5;              //Boltzmann Const. (micron pN/K)
+double TEMPERATURE = 300.0;             //Kelvin
+double dt = 0.0005;                     //time step (seconds)
+
+
 
 int main(){
 
     cout << "Hello I am in main" << endl;
+    cout << "My Boltz constant is: " << kB << endl;
+    cout << "My theta is: " << THETA_EQUI_ANGLE << endl;
+    cout << "Pi is: " << acos(-1.0) << endl;
 
 
     return 0;
